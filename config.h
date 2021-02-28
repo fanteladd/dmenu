fcf99dd72e35581819d5019447450827184f1739
Davide <davidetoni@protonmail.com>
2021-02-28 17:17:43 +0100

    inital release

---
+++ config.h
@@ -0,0 +1,31 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"SF Pro Text:style=Medium:size=11.5",
+  "Material Design Icons Desktop:style=Regular:size=13"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#e2e2e4", "#262626" },
+	[SchemeSel] = { "#e2e2e4", "#fe8019" },
+  [SchemeSelHighlight] = { "#ebdbb2", "#ff6565" },
+  [SchemeNormHighlight] = { "#ffc978", "#ff6565" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+  [SchemeOutHighlight] = { "#ffc978", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 27;
+static unsigned int min_lineheight = 8;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
