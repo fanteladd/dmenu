d9c89eaa581aeac331ac0477a616fc11dfb005f7
Davide <davidetoni@protonmail.com>
2021-03-24 01:03:17 +0100

    added xresources support

--- config.def.h
+++ config.def.h
@@ -5,23 +5,23 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"SF Pro Text:style=Medium:size=11.5",
-  "Material Design Icons Desktop:style=Regular:size=13"
+	"SF Pro Text:style=Medium:size=12",
+    "Material Design Icons:style=Regular:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#e2e2e4", "#262626" },
+	[SchemeNorm] = { "#e2e2e4", "#121212" },
 	[SchemeSel] = { "#e2e2e4", "#fe8019" },
-  [SchemeSelHighlight] = { "#ebdbb2", "#ff6565" },
-  [SchemeNormHighlight] = { "#ffc978", "#ff6565" },
+  [SchemeSelHighlight] = { "#ffb366", "#98971a" },
+  [SchemeNormHighlight] = { "#ffb366", "#5b727c" },
 	[SchemeOut] = { "#000000", "#00ffff" },
   [SchemeOutHighlight] = { "#ffc978", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 23;
+static unsigned int lineheight = 25;
 static unsigned int min_lineheight = 8;
 
 /*
